3521a82a199cf4464bc2c098c60f759b4aa4f9ed
Raul Reyes malqui <rerm1992@gmail.com>
2016-01-12 15:40:10 +1300

    Form validation for contact us section. Form is validated with respective to the field type. Changes in routes to send user to thank you page once email is sent. Added Text Area included classes for home and about view.

--- routes.php
+++ routes.php
@@ -4,31 +4,107 @@ $page = isset($GET['page']) ? $_GET['page'] : 'home';
 
 switch ($page) {
 	case 'home':
+		if (isset($_SESSION['feedbackandquestions'])) {
+			$feedbackandquestions = $_SESSION['feedbackandquestions'];
+		} else {
+			$_SESSION['feedbackandquestions'] = NULL;
+			$feedbackandquestions = [
+				'email' => "",
+				'subject' => "",
+				'message' => "",
+				'newsletter' => "",
+				'errors'=> [
+					'email'=>"",
+					'subject'=>"",
+					'message'=>"",
+					'newsletter'=>""
+				]
+			];
+		}
+
 		require "classes/HomeView.php";
-		$view = new HomeView();
+
+		$view = new HomeView($feedbackandquestions);
 		$view->render();
 		break;
+
+	case 'feedbackandquestions':
+		$_SESSION['feedbackandquestionserror'] = NULL;
+		$feedbackandquestions = [ 'errors' => []
+								];
+		$expectedVariables = ['email', 'subject', 'message', 'newsletter'];
+
+		foreach ($expectedVariables as $variable) {
+			$feedbackandquestions['errors'][$variable]="";
+		}
+
+		if (isset($_POST[$variable])) {
+			$feedbackandquestions[$variable] = $_POST[$variable];
+		}else {
+			$feedbackandquestions[$variable] = "";
+
+		} }
+
+		//validating the form
+
+		$error = false;
+
+		
+		if(! filter_var($feedbackandquestions['email'], FILTER_VALIDATE_EMAIL)){
+			$feedbackandquestions['errors']['email']="Enter a valid email address";
+			$error = true;
+		}
+
+
+		if(strlen($feedbackandquestions['subject']) == 0) {
+			$feedbackandquestions['errors']['subject']= "Enter a subject title for your message";
+			$error = true;
+		}
+
+		if(strlen($feedbackandquestions['message']) == 0) {
+			$feedbackandquestions['errors']['message']= "Remember to type out your feedback and/ or question";
+			$error = true;
+		}
+
+
+		if( $error === true){
+			$_SESSION['suggestmovieerror'] = true;
+			$_SESSION['moviesuggest'] = $moviesuggest;
+			header("Location:./#moviesuggest");
+			exit();
+		}
+		echo "Successfully suggested a movie.";
+		break;
+
+
+		break;
+
 	case 'buyAdSpace':
 		include "templates/buyadspace.inc.php";
 		break;
+
 	case 'SellAdSpace':
 		include "templates/selladspace.inc.php";
 		break;
+
 	case 'login':
 		include "templates/login.inc.php";
 		break;
+
 	case 'about':
-		include "templates/about.inc.php";
 		require "classes/AboutView.php";
 		$view = new AboutView();
 		$view->render();
 		break;
+
 	case 'statistics&press':
 		include "templates/stats&press.inc.php";
 		break;
+
 	case 'advertisers':
 		include "templates/advertisers.inc.php";
 		break;
+
 	case 'publishers':
 		include "templates/publishers.inc.php";
 		break;
--- templates/index.inc.php
+++ templates/index.inc.php
@@ -79,21 +79,63 @@
     <section id="contact" class="container content-section text-center">
         <div class="row">
             <div class="col-lg-8 col-lg-offset-2">
-                <h2>Contact </h2>
-                <p>Feel free to email us to provide some feedback on our templates, give us suggestions for new templates and themes, or to just say hello!</p>
-                <p><a href="mailto:feedback@startbootstrap.com">feedback@startbootstrap.com</a>
+               
+              <form id="feedbackandquestions" action=".\?page=feedbackandquestions" method="POST" class="form-horizontal">
+               <h2 class="text-center">Contact Us</h2>
+               <p>Feel free to email us to ask us about any general enquiries</p>
+
+                <div class="form-group <?php if($errors['email']): ?> has-error <?php endif; ?>">
+                    <label for="emailaddress" >Email</label>
+                      <input type="email" class="form-control" id="emailaddress" name="email" placeholder="example@mail.com"
+                      value="<?php echo $email; ?>">
+                      <div class="help-block"><?php echo $errors['email']; ?></div>
+                </div>
+
+                <div class="form-group <?php if($errors['subject']): ?> has-error <?php endif; ?>">
+                    <label for="messagesubject" >Subject</label>
+                      <input class="form-control" id="messagesubject" name="subject" placeholder="question regarding service , feedback about site usability, media enquiries"
+                      value="<?php echo $subject; ?>">
+                      <div class="help-block"><?php echo $errors['subject']; ?></div>
+                  </div>
+
+                <div class="form-group <?php if($errors['message']): ?> has-error <?php endif; ?>">
+                    <label for="messagecontent" >Message</label>
+                    <textarea class="form-control" rows="3" id="messagecontent" name="message" placeholder="Please enter your message here."
+                    value="<?php echo $message; ?>"> </textarea>
+                    <div class="help-block"><?php echo $errors['message']; ?></div>
+                  </div>
+
+                      
+                  <div class="form-group">
+                      <div class="checkbox">
+                        <label>
+                          <input id="newsletter" name="newsletter" type="checkbox" 
+                          <?php if($newsletter): ?>
+                            checked
+                          <?php endif; ?> value="yes"> Sign Up for Schlocktoberfest Newsletter <small> Once a month!</small>
+                        </label>
+                    </div>
+                  </div>
+
+                <div class="form-group">
+                  <div class="col-lg-8 col-lg-offset-2">
+                    <button class="btn btn-primary">Send Message</button>
+                  </div>
+                </div>
+                      
                 </p>
                 <ul class="list-inline banner-social-buttons">
                     <li>
                         <a href="https://twitter.com/SBootstrap" class="btn btn-default btn-lg"><i class="fa fa-twitter fa-fw"></i> <span class="network-name">Twitter</span></a>
                     </li>
                     <li>
-                        <a href="https://github.com/IronSummitMedia/startbootstrap" class="btn btn-default btn-lg"><i class="fa fa-github fa-fw"></i> <span class="network-name">Github</span></a>
+                        <a href="https://facebook.com/" class="btn btn-default btn-lg"><i class="fa fa-facebook fa-fw"></i> <span class="network-name">Facebook</span></a>
                     </li>
                     <li>
                         <a href="https://plus.google.com/+Startbootstrap/posts" class="btn btn-default btn-lg"><i class="fa fa-google-plus fa-fw"></i> <span class="network-name">Google+</span></a>
                     </li>
                 </ul>
+                </form> 
             </div>
         </div>
     </section>
